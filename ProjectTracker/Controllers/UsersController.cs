45c125f677c3698ec305c0253d20d1ca5a024b97
taiielmehdi <m.taii@peaqock.com>
2022-08-26 23:17:02 +0100

    Adding Controllers

---
+++ ProjectTracker/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTracker.Mvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
